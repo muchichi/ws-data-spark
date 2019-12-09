8311a8480a02ec250a50b0e79e0b3c3c5fc89dfb
Mussie Nadew <muchichi@gmail.com>
2019-12-08 22:43:17 -0500

    Simplifiy the entry point by modularizing and breaking down the steps to solve the issue.

--- data_processing.py
+++ data_processing.py
@@ -1,13 +1,17 @@
 from shared.Utils import Common
-
+import pandas as pd
 
 if __name__ == '__main__':
 	
 	locations = Common.get_df('data/DataSample.csv').drop_duplicates([' TimeSt', 'Latitude', 'Longitude'])
-	locations.printSchema()
-	print(locations.count())
-	locations.show(5)
+
+	new_locations = Common.assign_poi(locations)
+	new_locations.show(250)
+	# Common.test()
 	
-	
+	# ls = [{'poi': 'POI4', 'distance': 834.3764352890981}, {'poi': 'POI2', 'distance': 812.3764352890981}, {'poi': 'POI1', 'distance': 857.3764352890981}, {'poi': 'POI3', 'distance': 810.3764352890981}]
+	# df = pd.DataFrame(ls).sort_values('distance')
+	# print(df)
+	# print(df['poi'].values[0])
 	
 
